50cc9e332a61552fd2c711d04608ceed4113ef20
Kristoffer Rigbolt <kri@gubra.dk>
2017-06-30 14:02:34 +0200

    Test R commit

--- R/hello.R
+++ R/hello.R
@@ -1,53 +1,40 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
 
 hello <- function() {
   print("Hello, world!")
   require(devtools)
 
 }
+
 updatePackageVersion <- function(packageLocation ="."){
-  ## Read DESCRIPTION file
   desc <- readLines(file.path(packageLocation, "DESCRIPTION"))
-
-  ## Find the line where the version is defined
   vLine <- grep("^Version\\:", desc)
-
-  ## Extract version number
+  dLine = grep("^Date\\:", desc)
+  if(length(dLine) == 0){
+    desc = c(desc, paste("Date:", Sys.Date()))
+  }else{
+    desc[dLine] = paste("Date:", Sys.Date())
+  }
   vNumber <- gsub("^Version\\:\\s*", "", desc[vLine])
-
-    ## Split the version number into two; a piece to keep, a piece to increment
   versionNumber <- strsplit(vNumber, "\\.")[[1]]
   versionParts <- length(versionNumber)
   vNumberKeep <- paste(versionNumber[1:(versionParts-1)], sep= "", collapse= ".")
   vNumberUpdate <- versionNumber[versionParts]
-
-  ## Replace old version number with new one (increment by 1)
   oldVersion <- as.numeric(vNumberUpdate)
   newVersion <- oldVersion + 1
-
-  ## Build final version number
   vFinal <- paste(vNumberKeep, newVersion, sep = ".")
-
-  ## Update DESCRIPTION file (in R)
   desc[vLine] <- paste0("Version: ", vFinal )
-
-  ## Update the actual DESCRIPTION file
   writeLines(desc, file.path(packageLocation, "DESCRIPTION"))
-library(devtools)
-  compileAttributes()
-   ## Return the updated version number to screen
-  return(vFinal)
+ return(vFinal)
 }
+library(git2r)
+library(adapr)
+
+files = dir(".", recursive = T)
+exts = tools::file_ext (files)
+files = files[!exts %in% c("Rproj")]
+repo = init(".")
+add(repo, files)
+
+commit(repo, "Test R commit")
+default_signature(repo)
+?git.commit
