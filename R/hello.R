32a43a091ccba620c467f5a8e8407274e883077d
Kristoffer Rigbolt <kri@gubra.dk>
2017-06-30 09:28:33 +0200

    Added R/

---
+++ R/hello.R
@@ -0,0 +1,53 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+  require(devtools)
+
+}
+updatePackageVersion <- function(packageLocation ="."){
+  ## Read DESCRIPTION file
+  desc <- readLines(file.path(packageLocation, "DESCRIPTION"))
+
+  ## Find the line where the version is defined
+  vLine <- grep("^Version\\:", desc)
+
+  ## Extract version number
+  vNumber <- gsub("^Version\\:\\s*", "", desc[vLine])
+
+    ## Split the version number into two; a piece to keep, a piece to increment
+  versionNumber <- strsplit(vNumber, "\\.")[[1]]
+  versionParts <- length(versionNumber)
+  vNumberKeep <- paste(versionNumber[1:(versionParts-1)], sep= "", collapse= ".")
+  vNumberUpdate <- versionNumber[versionParts]
+
+  ## Replace old version number with new one (increment by 1)
+  oldVersion <- as.numeric(vNumberUpdate)
+  newVersion <- oldVersion + 1
+
+  ## Build final version number
+  vFinal <- paste(vNumberKeep, newVersion, sep = ".")
+
+  ## Update DESCRIPTION file (in R)
+  desc[vLine] <- paste0("Version: ", vFinal )
+
+  ## Update the actual DESCRIPTION file
+  writeLines(desc, file.path(packageLocation, "DESCRIPTION"))
+library(devtools)
+  compileAttributes()
+   ## Return the updated version number to screen
+  return(vFinal)
+}
